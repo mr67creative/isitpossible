c3c7d9e6e9cad36c60dc53cc0a49b8c0368d0ca7
mr67creative <mr67creative@gmail.com>
2026-09-08 17:49:23 +0545

    defined the layout of the dock

--- src/components/Dock.tsx
+++ src/components/Dock.tsx
@@ -1,8 +1,18 @@
 function Dock() {
   return (
-    <div className='absolute bottom-0 left-0 w-[40vw] mx-[30vw] border mb-4 rounded-full h-10 px-3 flex flex-row items-center justify-between z-20'>
-      Dock
-    </div>
+    <footer className='absolute bottom-0 left-0 w-[40vw] mx-[30vw] mb-4 rounded-full h-10 px-3 flex flex-row items-center justify-between z-20 bg-blue-500'>
+      <div className="relative w-full flex flex-row items-center justify-between">
+        <section>
+          APPS
+        </section>
+        <section className="absolute right-1/2 translate-x-1/2 rounded-full h-16 w-16 bg-blue-500 flex flex-row items-center justify-center">
+          LOGO
+        </section>
+        <section>
+          APPS
+        </section>
+      </div>
+    </footer>
   )
 }
 
