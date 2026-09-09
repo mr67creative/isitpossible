68acc732ec1811dddb5f61b06eb304e8fc2bd6f7
mr67creative <mr67creative@gmail.com>
2026-09-09 21:49:24 +0545

    created useScreen store to consume the chat dialogs with ease

---
+++ src/context/screen.tsx
@@ -0,0 +1,32 @@
+import { create } from "zustand";
+import { immer } from "zustand/middleware/immer";
+
+type ChatContent = {
+    somebody: string;
+    me: () => React.ReactNode;
+};
+
+const chatContent: Record<string, ChatContent | ChatContent[]> = {
+    "introduction": {
+        somebody: "Who the HELL is Possible?",
+        me: () => <p>Sorry to say but its me (aka Sambhav Aryal)</p>
+    },
+    "handle": {
+        somebody: "What do you handle?",
+        me: () => <a href="https://github.com/mr67creative">I am handled by Github lol</a>
+    }
+}
+
+interface Screen {
+    totalAppOpened: number;
+    defaultChatContent: Record<string, ChatContent | ChatContent[]>;
+    chat: Record<string, ChatContent | ChatContent[]>;
+}
+
+export const useScreen = create<Screen>()(
+    immer(set => ({
+        totalAppOpened: 0,
+
+        defaultChatContent: chatContent,
+        chat: {}
+    })))
